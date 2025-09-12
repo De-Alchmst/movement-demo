48b7166dfde1a4eb2e33bf9d3bd15f6166812ff5
De-Alchmst <PolakJ_37@proton.me>
2025-09-12 13:11:13 +0200

    intro text

--- src/intro.scm
+++ src/intro.scm
@@ -6,13 +6,17 @@
   (define font-size 30)
   (define font-spacing 0)
 
+  (define intro-text
+    "Jsi student ZCU, ale kvůli včerejší pařbě jsi zapoměl
+že je neděle, tudíž jsi pařil ještě víc než obvykle.
+Dnes jsi v důsledku tohoto omylu zaspal.
+Rychle! Obleč se, najdi brýle, pouzdro na ně a batoh
+a běž na zastávku, ať chytíš alespoň další tramvaj!")
+
   (define (show-intro)
     (draw
-      (clear-background raywhite)
-      (draw-text-ex fira-code
-                    "Ahoj, Světe!"
-                    text-pos font-size font-spacing
-                    black))
+     (clear-background raywhite)
+     (draw-text-ex fira-code intro-text text-pos font-size font-spacing black))
 
     (cond
       ((window-should-close?)
